ba41e6200e926a8dd6a0309a97a9e6c56992c4e3
dan-dye <danrdye@gmail.com>
2022-04-26 13:52:24 -0400

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedItems) {
+  let results = array;
+  removedItems.forEach(removedItem => results = results.filter(resultItem => resultItem !== removedItem));
+  return results;
 };
 
 // Do not edit below this line
