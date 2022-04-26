8f1ff4e46c3cf62f65f2aeada5d2391d0eb39825
dan-dye <danrdye@gmail.com>
2022-04-26 14:14:42 -0400

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let total = 0;
+  let startNum = num1;
+  let endNum = num2;
+  if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return 'ERROR'
+  }
+  if(num1 < 0 || num2 < 0) {
+    return 'ERROR'
+  }
+  if(num1 > num2) {
+    startNum = num2;
+    endNum = num1;
+  }
+  for (let i = startNum; i <= endNum; i++) {
+    total = total + i;
+  }
+  return total;
 };
 
 // Do not edit below this line
